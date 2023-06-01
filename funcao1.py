a19963f365af9825331df65de8dd1db73092214c
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-05-31 21:06:58 -0300

    Create funcao1.py

---
+++ funcao1.py
@@ -0,0 +1,14 @@
+"""
+1) Faça uma função que receba três numeros inteiros: a,b e c, onde 'a' é maior que 1.
+A função deve somar todos os inteiros entre b e c que sejam divisiveis por 'a' (inclusive b e c)
+e retornar o resultado para a função principal.
+"""
+def somanumeros(a:int, b:int, c:int):
+    soma = 0
+    for i in range(b, c+1):
+        if i % a == 0:
+            soma = soma + i
+    return soma
+
+guardar = somanumeros(10,100,2)
+print(guardar)
